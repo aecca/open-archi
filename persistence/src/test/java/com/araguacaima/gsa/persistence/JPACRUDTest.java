294be3d25f9d2874c40ce087e08088743bfb903a
araguacaima <araguacaima@gmail.com>
2017-10-20 10:45:11 -0600

    Initial commit

--- persistence/src/test/java/com/araguacaima/gsa/persistence/JPACRUDTest.java
+++ persistence/src/test/java/com/araguacaima/gsa/persistence/JPACRUDTest.java
@@ -3,6 +3,7 @@ package com.araguacaima.gsa.persistence;
 import com.araguacaima.gsa.persistence.msa.Msa;
 import org.junit.Test;
 
+import javax.persistence.TypedQuery;
 import java.util.Calendar;
 import java.util.List;
 
@@ -40,14 +41,19 @@ public class JPACRUDTest extends AbstractJPATest {
     public void testPersist_success() {
         em.getTransaction().begin();
         final Msa msa = new Msa();
+        String id = msa.getId();
+        msa.setIssueDate(Calendar.getInstance().getTime());
         msa.setExpirationDate(Calendar.getInstance().getTime());
         em.persist(msa);
         em.getTransaction().commit();
 
-        List<Msa> msas = em.createNamedQuery("Msa.getAll", Msa.class).getResultList();
+        TypedQuery<Msa> consultaAlumnos = em.createNamedQuery("Msa.getById", Msa.class);
+        consultaAlumnos.setParameter("id", id);
+        List<Msa> msas = consultaAlumnos.getResultList();
 
         assertNotNull(msas);
-        assertEquals(2, msas.size());
+        assertEquals(1, msas.size());
+        assertEquals(id, msas.get(0).getId());
     }
 
 }
