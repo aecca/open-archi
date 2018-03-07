aff7d108c5c2eb0d35876a86939c3794e247c132
Alex <alejandromanuel.mendez.aragua@bbva.com>
2018-03-07 13:57:45 -0600

    Adding palettes

--- web/src/main/resources/web/public/javascripts/menu.js
+++ web/src/main/resources/web/public/javascripts/menu.js
@@ -1,7 +1,7 @@
 function showMenu() {
 
     // when the page loads, change the class of navigation LI's
-    const lis = document.getElementById("sectionsExamples").getElementsByTagName("li");
+    const lis = document.getElementById("sectionsExamples").querySelectorAll("li");
     const l = lis.length;
     for (let i = 0; i < l; i++) {
         const anchor = lis[i].childNodes[0];
@@ -15,10 +15,12 @@ function showMenu() {
             const span = document.createElement('span');
             span.className = "samplespan";
             const img = document.createElement('img');
-            img.height = "200";
+            img.height = "100";
             img.src = "/images/screenshots/" + imgname + ".png";
             span.appendChild(img);
             anchor.appendChild(span);
+        } else {
+            lis[i].childNodes[0].remove();
         }
     }
 }
