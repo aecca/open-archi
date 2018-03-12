01d4f4688414a303e38edb6c30d24464a9e02f6d
Alex <alejandromanuel.mendez.aragua@bbva.com>
2018-03-12 10:22:20 -0600

    Main menu

--- persistence/diagrams/core/src/main/java/com/araguacaima/open_archi/persistence/diagrams/core/Taggable.java
+++ persistence/diagrams/core/src/main/java/com/araguacaima/open_archi/persistence/diagrams/core/Taggable.java
@@ -5,6 +5,7 @@ import org.hibernate.annotations.DynamicUpdate;
 import org.springframework.stereotype.Component;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 import java.util.LinkedHashSet;
 import java.util.Set;
 
@@ -16,16 +17,25 @@ import java.util.Set;
 @DiscriminatorColumn(name = "modelType", discriminatorType = DiscriminatorType.STRING)
 @NamedQueries({@NamedQuery(name = Taggable.GET_ALL_MODELS,
         query = "select a from Taggable a "), @NamedQuery(name = Taggable.GET_MODELS_BY_TYPE,
-        query = "select a from Taggable a where TYPE(a)=:modelType")})
+        query = "select a from Taggable a where TYPE(a)=:modelType"),
+        @NamedQuery(name = Taggable.GET_MODELS_BY_STATUS,
+        query = "select a from Taggable a where a.status=:status")})
 @Component
 public class Taggable extends BaseEntity {
 
     public static final String GET_ALL_MODELS = "get.all.models";
     public static final String GET_MODELS_BY_TYPE = "get.models.by.type";
+    public static final String GET_MODELS_BY_STATUS = "get.models.by.status";
 
     @ElementCollection
     @CollectionTable(name = "Tags", schema = "DIAGRAMS")
     protected Set<String> tags = new LinkedHashSet<>();
+
+
+    @Column(nullable = false)
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    private Status status = Status.INITIAL;
 
     public Set<String> getTags() {
         return tags;
@@ -33,6 +43,14 @@ public class Taggable extends BaseEntity {
 
     public void setTags(Set<String> tags) {
         this.tags = tags;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
     }
 
     public void override(Taggable source) {
