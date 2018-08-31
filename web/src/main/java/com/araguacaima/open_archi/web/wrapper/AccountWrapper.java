4fd3a851ac57253f9d3cca21845f8c60dedab9ca
Alex <alejandromanuel.mendez.aragua@bbva.com>
2018-08-31 09:03:04 -0500

    Starting authentication & authorization

--- web/src/main/java/com/araguacaima/open_archi/web/wrapper/AccountWrapper.java
+++ web/src/main/java/com/araguacaima/open_archi/web/wrapper/AccountWrapper.java
@@ -3,20 +3,20 @@ package com.araguacaima.open_archi.web.wrapper;
 import com.araguacaima.open_archi.persistence.meta.Account;
 import com.araguacaima.open_archi.persistence.meta.Avatar;
 import org.pac4j.core.profile.UserProfile;
+import org.pac4j.oauth.profile.google2.Google2Profile;
+
+import java.net.URL;
 
 public class AccountWrapper {
     public static Account toAccount(UserProfile profile) {
         Account account = null;
         if (profile != null) {
             account = new Account();
-            account.setName((String) profile.getAttribute("given_name"));
-            account.setLastname((String) profile.getAttribute("family_name"));
-            account.setLogin((String) profile.getAttribute("name"));
-            account.setEmail((String) profile.getAttribute("email"));
-            String url = (String) profile.getAttribute("picture");
-            if (url == null) {
-                url = (String) profile.getAttribute("image.url");
-            }
+            account.setName(((Google2Profile) profile).getFamilyName());
+            account.setLastname(((Google2Profile) profile).getFamilyName());
+            account.setLogin(((Google2Profile) profile).getDisplayName());
+            account.setEmail(((Google2Profile) profile).getEmail());
+            String url = profile.getAttribute("image.url").toString();
             Avatar avatar = new Avatar();
             avatar.setUrl(url);
             account.setAvatar(avatar);
